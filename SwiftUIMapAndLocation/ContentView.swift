693d7779463d475d1ae0517a5d7db5c217635261
Nadia Hansen <nadiahansen.kea@gmail.com>
2020-10-02 08:35:11 +0200

    Initial Commit

---
+++ SwiftUIMapAndLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMapAndLocation
+//
+//  Created by Nadia Hansen on 02/10/2020.
+//  Copyright © 2020 Nadia Hansen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMapAndLocationTests/SwiftUIMapAndLocationTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIMapAndLocationTests.swift
+//  SwiftUIMapAndLocationTests
+//
+//  Created by Nadia Hansen on 02/10/2020.
+//  Copyright © 2020 Nadia Hansen. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUIMapAndLocation
+
+class SwiftUIMapAndLocationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
